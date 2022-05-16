bbcfaec2bb383a3a940d70a056fa890b971e27e6
Ismail Khairy Shepl <som3ah@Ismails-MacBook-Pro-2.local>
2022-05-16 03:56:22 +0200

    init commit

---
+++ src/main/java/com/example/demo/HerukoTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerukoTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerukoTest2Application.class, args);
+	}
+
+}
